135fae3780a33886ae3299da5e3ffec5e4001a86
Naumanlatif1994 <naumanlatif99@gmail.com>
2022-10-20 16:10:41 +0500

    Testng project

---
+++ Testng/src/Testngbasics/day1.java
@@ -0,0 +1,13 @@
+package Testngbasics;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+
+	@Test
+	public void Homeloan()
+	{
+		System.out.println("Home Loan");
+	}
+	
+}
---
+++ Testng/src/Testngbasics/day2.java
@@ -0,0 +1,18 @@
+package Testngbasics;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+@Test
+	public void Carloan_Login()
+	{
+		System.out.println("Enter User Name");
+		System.out.println("Enter Password");
+	}
+@Test
+public void Carloan_Home()
+{
+	System.out.println("Welcome");
+}
+
+}
